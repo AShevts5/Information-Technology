cba08269dd3b87aa24dec5628c77825d3c064217
Anna_Shevtsova9775 <annasevcova01@gmail.com>
2026-01-30 10:05:10 +0300

    Update HelloWorld_5.java

--- HelloWorld_5.java
+++ HelloWorld_5.java
@@ -3,11 +3,18 @@ public class HelloWorld {
      * @param args
      */
     public static void main(String[] args){
-        Cat ourcat = new Cat();
+        /*Cat ourcat = new Cat();
         ourcat.eat();
         ourcat.sleep();
         String say = ourcat.speak("Play with me");
+        System.out.println(say);*/
+
+        Dog ourdog = new Dog();
+        ourdog.eat();
+        ourdog.sleep();
+        String say = ourdog.speak("Play with me");
         System.out.println(say);
+        ourdog.play();
 
     }
 }
