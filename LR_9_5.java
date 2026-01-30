cd8485088eed974f53ced04c27fa0f810922dbf1
Anna_Shevtsova9775 <annasevcova01@gmail.com>
2026-01-30 14:31:06 +0300

    Create LR_9_5.java

---
+++ LR_9_5.java
@@ -0,0 +1,10 @@
+public class NIneFive {
+    public static void main(String[] args) {
+        int k = 0, i = 0;
+        do {
+            System.out.println(k);
+            i += 1;
+            k += -5;
+        } while (i != 10);
+    }
+}
