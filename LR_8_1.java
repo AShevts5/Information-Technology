354be95d6d49bd069c52a216630133a68d85aa5f
Anna_Shevtsova9775 <annasevcova01@gmail.com>
2026-01-30 11:41:30 +0300

    Update LR_8_1.java

--- LR_8_1.java
+++ LR_8_1.java
@@ -1,4 +1,5 @@
-public class Eight {
+//Первый способ - на конкретных числах
+/*public class Eight {
     public static void main(String[] args){
         int a = 5, b = 7, c = 100, d = 99;
         int min0 = 1000000000;
@@ -12,5 +13,30 @@ public class Eight {
             min0 = d;
 
         System.out.println("Наименьшее число = " + min0);
+    }
+}*/
+
+//Второй способ - с вводом чисел от пользователя
+import java.util.Scanner;
+public class Eight {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите 4 целых числа:");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int min0 = 1000000000;
+        if (a < min0)
+            min0 = a;
+        if (b < min0)
+            min0 = b;
+        if (c < min0)
+            min0 = c;
+        if (c < min0)
+            min0 = d;
+
+        System.out.println("Наименьшее число = " + min0);
+
     }
 }
