4a1a4d428d7142fd214ea576361180c74e601787
Anna_Shevtsova9775 <annasevcova01@gmail.com>
2026-01-30 09:31:31 +0300

    Create Cat_5.java

---
+++ Cat_5.java
@@ -0,0 +1,18 @@
+public class Cat {
+    private int weight; // вес кота
+    private String name; // имя
+    private String color; // цвет
+    // кот ест
+    public void eat() {
+        System.out.print("Eating...\n");
+    }
+    // кот спит
+    public void sleep() {
+        System.out.print("Sleeping zz-z-z-z...\n");
+    }
+    // кот говорит
+    public String speak(String words) {
+        String phrase = words + "...mauu...\n";
+        return phrase;
+    }
+}
